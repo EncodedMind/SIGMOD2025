7cf640e776862b4039fe161fdb78975f925f38df
VangelisVas <evagelos.vass@gmail.com>
2025-10-30 16:59:37 +0800

    Updated the Tests for Robin Hood Hashing

--- robinhood.cpp
+++ robinhood.cpp
@@ -1,123 +1,116 @@
 // Robin Hood Hashing
 #include <iostream>
-#include "robinhood.hpp"
+#include <vector>
 #include <bit>
 using namespace std;
 
-Robinhood::Robinhood(int size)
-{ // constructor
-    N = bit_ceil(static_cast<unsigned>(size));
-    hashtable.resize(N);
-}
+struct Robinhood{
+    int N;
 
-int Robinhood::hash_function(int key) const
-{
-    return key & (N - 1);
-}
+    struct Entry{
+        int key;
+        vector<int> values;
+        int psl;
 
-void Robinhood::insert(int key, const vector<int> &inputvalues)
-{
-    int pos = hash_function(key);
-    int psl = 0;
-    vector<int> values = inputvalues;
+        Entry(int k = -1, const vector<int>& vec = {}, int p = -1) : key(k), values(vec), psl(p){};
+    };
 
-    while (1)
-    {
-        if (hashtable[pos].key == -1)
-        { // empty spot
-            hashtable[pos] = Entry(key, values, psl);
-            break;
-        }
-        else
-        {
-            if (hashtable[pos].key == key)
-            { // key already exists, just add the value
-                for (auto &v : values)
-                    hashtable[pos].values.push_back(v);
+    vector<Entry> hashtable;
+
+    Robinhood(int size){ // constructor
+        N = bit_ceil(static_cast<unsigned>(size));
+        hashtable.resize(N);
+    }
+
+    int hash_function(int key) const{
+        return key & (N-1);
+    }
+
+    void insert(int key, const vector<int>& inputvalues){
+        int pos = hash_function(key);
+        int psl = 0;
+        vector<int> values = inputvalues;
+
+        while(1){
+            if(hashtable[pos].key == -1){ // empty spot
+                hashtable[pos] = Entry(key, values, psl);
                 break;
             }
-            if (psl > hashtable[pos].psl)
-            {
-                Entry temp = hashtable[pos];
-                hashtable[pos] = Entry(key, values, psl);
-                key = temp.key;
-                values = temp.values;
-                psl = temp.psl;
+            else{
+                if(hashtable[pos].key == key){ // key already exists, just add the value
+                    for(auto& v : values) hashtable[pos].values.push_back(v);
+                    break;
+                }
+                if(psl > hashtable[pos].psl){
+                    Entry temp = hashtable[pos];
+                    hashtable[pos] = Entry(key, values, psl);
+                    key = temp.key;
+                    values = temp.values;
+                    psl = temp.psl;
+                }
+                psl++;
+                if(++pos == N) pos = 0; // in case the table ends, we have to go check at the beginning
             }
+        }
+    }
+
+    bool find(int key) const{
+        int pos = hash_function(key);
+        int psl = 0;
+
+        while(1){
+            if(hashtable[pos].key == key) return true; // found
+            if(hashtable[pos].key == -1) return false; // empty spot
+            if(psl > hashtable[pos].psl) return false; // if the psl of the key we are looking for is greater than the psl of the current element, then it means that the key is not in the table
             psl++;
-            if (++pos == N)
-                pos = 0; // in case the table ends, we have to go check at the beginning
+            if(++pos == N) pos = 0; // in case the table ends, we have to go check at the beginning
         }
     }
-}
 
-bool Robinhood::find(int key) const
-{
-    int pos = hash_function(key);
-    int psl = 0;
-
-    while (1)
-    {
-        if (hashtable[pos].key == key)
-            return true; // found
-        if (hashtable[pos].key == -1)
-            return false; // empty spot
-        if (psl > hashtable[pos].psl)
-            return false; // if the psl of the key we are looking for is greater than the psl of the current element, then it means that the key is not in the table
-        psl++;
-        if (++pos == N)
-            pos = 0; // in case the table ends, we have to go check at the beginning
-    }
-}
-
-void Robinhood::print() const
-{
-    cout << "Final table:\n";
-    for (int i = 0; i < N; ++i)
-    {
-        const auto &[key, values, psl] = hashtable[i];
-        cout << "Bucket " << i << ": ";
-        if (key == -1)
-            cout << "{Empty} ";
-        else
-        {
-            cout << "{" << key << ": [";
-            for (auto v : values)
-                cout << v << " ";
-            cout << "]} ";
+    void print() const{
+        cout << "Final table:\n";
+        for(int i=0; i<N; ++i){
+            const auto& [key, values, psl] = hashtable[i];
+            cout << "Bucket " << i << ": ";
+            if(key == -1) cout << "{Empty} ";
+            else{
+                cout << "{" << key << ": [";
+                for(auto v : values) cout << v << " ";
+                cout << "]} ";
+            }
+            cout << "- Psl: " << psl << '\n';
         }
-        cout << "- Psl: " << psl << '\n';
     }
-}
+};
 
-// int main(){
+int main(){
     
-//     Robinhood hashtable(11);
-//     vector<pair<int,int>> data = {
-//         {16, 160},   // hashes to 0
-//         {32, 320},   // hashes to 0, forces collision
-//         {48, 480},   // hashes to 0, second collision
-//         {1, 10},     // hashes to 1
-//         {17, 170},   // hashes to 1, collision
-//         {2, 20},     // hashes to 2
-//         {18, 180},   // hashes to 2, collision
-//         {16, 161},   // duplicate key, appends to vector
-//         {32, 321},   // duplicate key, appends to vector
-//         {63, 630},    // hashes to 15, last bucket
-//         {79, 790}    // hashes to 15, last bucket → tests wrap-around
-//     };
+    Robinhood hashtable(11);
+    vector<pair<int,int>> data = {
+        {16, 160},   // hashes to 0
+        {32, 320},   // hashes to 0, forces collision
+        {48, 480},   // hashes to 0, second collision
+        {1, 10},     // hashes to 1
+        {17, 170},   // hashes to 1, collision
+        {2, 20},     // hashes to 2
+        {18, 180},   // hashes to 2, collision
+        {16, 161},   // duplicate key, appends to vector
+        {32, 321},   // duplicate key, appends to vector
+        {63, 630},    // hashes to 15, last bucket
+        {79, 790}    // hashes to 15, last bucket → tests wrap-around
+    };
 
 
-//     for(auto [key, value] : data){
-//         hashtable.insert(key, {value});
-//     }
+    for(auto [key, value] : data){
+        hashtable.insert(key, {value});
+    }
 
-//     hashtable.print();
+    hashtable.print();
 
-//     cout << (hashtable.find(9) ? "Found 9\n" : "9 not found\n");
-//     cout << (hashtable.find(100) ? "Found 100\n" : "100 not found\n");
+    cout << (hashtable.find(9) ? "Found 9\n" : "9 not found\n");
+    cout << (hashtable.find(100) ? "Found 100\n" : "100 not found\n");
 
-//     return 0;
-// }
+    return 0;
+}
 
 // g++ -std=c++20 robinhood.cpp -o robinhood && ./robinhood
--- tests/hash_tests.cpp
+++ tests/hash_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <table.h>
 #include <plan.h>
+#include "robinhood.h"
 
 TEST_CASE("Empty join", "[join]") {
     Plan plan;
@@ -24,3 +25,239 @@ TEST_CASE("Empty join", "[join]") {
     REQUIRE(result.columns[0].type == DataType::INT32);
     REQUIRE(result.columns[1].type == DataType::INT32);
 }
+
+TEST_CASE("Robin Hood Hashing", "[robinhood]"){
+
+    using Robinhood = Hashalgorithm<int32_t>;
+
+    SECTION("Basic Insert and Find"){
+        // N = 16
+        Robinhood hashtable(11);
+
+        hashtable.insert(1, {static_cast<size_t>(10)});
+        hashtable.insert(16, {static_cast<size_t>(20)});
+
+        REQUIRE(!hashtable.find_values(1).empty());
+        REQUIRE(!hashtable.find_values(16).empty());
+
+        REQUIRE(hashtable.hashtable[1].key == 1);
+        REQUIRE(hashtable.hashtable[1].psl == 0);
+
+        REQUIRE(hashtable.hashtable[0].key == 16);
+        REQUIRE(hashtable.hashtable[0].psl == 0);
+    }
+
+    SECTION("Non Existant Key"){
+        Robinhood hashtable(11);
+
+        REQUIRE(hashtable.find_values(8).empty());
+
+        for(int i = 0; i < 11; ++i){
+            hashtable.insert(i, {static_cast<size_t>(i*10)});
+        }
+        
+        for(int i = 0; i < 11; ++i){
+            REQUIRE(!hashtable.find_values(i).empty());
+        }
+
+        REQUIRE(hashtable.find_values(11).empty());
+        REQUIRE(hashtable.find_values(16).empty());
+    }
+
+    SECTION("Hashtable Size"){
+        Robinhood hashtable(14);
+        REQUIRE(hashtable.N == 16);
+
+        Robinhood ht(16);
+        REQUIRE(ht.N == 16);
+
+        Robinhood high(200);
+        REQUIRE(high.N == 256);
+    }
+
+    SECTION("Find Test"){
+        Robinhood hashtable(8);
+
+        hashtable.insert(0, {static_cast<size_t>(0)});
+        hashtable.insert(1, {static_cast<size_t>(1)});
+        hashtable.insert(8, {static_cast<size_t>(8)});
+        hashtable.insert(9, {static_cast<size_t>(9)});
+        hashtable.insert(16, {static_cast<size_t>(16)});
+        hashtable.insert(17, {static_cast<size_t>(17)});
+
+        /*  keys: 0, 8, 16, 9, 1, 17
+            psl:  0, 1,  2, 2, 3,  4    */
+
+        // Find should terminate bc our psl > psl[pos]
+
+        // Hash 1
+        REQUIRE(hashtable.find_values(25).empty());
+        
+        // Hash 0
+        REQUIRE(hashtable.find_values(24).empty());
+
+    }
+
+    SECTION("Random Insertions"){
+        Robinhood hashtable(11);
+
+        // Hash = 0
+        hashtable.insert(16, {static_cast<size_t>(160)}); // pos = 0, psl = 0
+        hashtable.insert(32, {static_cast<size_t>(320)}); // pos = 1, psl = 1
+        hashtable.insert(48, {static_cast<size_t>(480)}); // pos = 2, psl = 2
+
+        // Hash = 1
+        hashtable.insert(1, {static_cast<size_t>(10)}); // pos = 3, psl = 2
+        hashtable.insert(17, {static_cast<size_t>(170)}); // pos = 4, psl = 3
+
+        // Hash = 2
+        hashtable.insert(2, {static_cast<size_t>(20)}); // pos = 5, psl = 3 
+        hashtable.insert(18, {static_cast<size_t>(180)}); // pos = 6, psl = 4 
+
+        REQUIRE(hashtable.hashtable[0].key == 16);
+        REQUIRE(hashtable.hashtable[0].psl == 0);
+        REQUIRE(hashtable.hashtable[1].key == 32);
+        REQUIRE(hashtable.hashtable[1].psl == 1);
+        REQUIRE(hashtable.hashtable[2].key == 48);
+        REQUIRE(hashtable.hashtable[2].psl == 2);
+        REQUIRE(hashtable.hashtable[3].key == 1);
+        REQUIRE(hashtable.hashtable[3].psl == 2);
+        REQUIRE(hashtable.hashtable[4].key == 17);
+        REQUIRE(hashtable.hashtable[4].psl == 3);
+        REQUIRE(hashtable.hashtable[5].key == 2);
+        REQUIRE(hashtable.hashtable[5].psl == 3);
+        REQUIRE(hashtable.hashtable[6].key == 18);
+        REQUIRE(hashtable.hashtable[6].psl == 4);
+    }
+
+    SECTION("Insertions with Identical Key"){
+        Robinhood hashtable(8);
+        REQUIRE(hashtable.N == 8);
+
+        for(int i = 0; i < 23; ++i){
+            hashtable.insert(0, {static_cast<size_t>(i*10)});
+        }
+
+        REQUIRE(!hashtable.find_values(0).empty());
+        REQUIRE(hashtable.hashtable[0].values.size() == 23);
+        REQUIRE(hashtable.hashtable[0].psl == 0);
+
+        for(int i = 0; i < 23; ++i){
+            REQUIRE(hashtable.hashtable[0].values[i] == static_cast<size_t>(i * 10));
+        }
+
+        hashtable.insert(1, {static_cast<size_t>(10)});
+        REQUIRE(hashtable.hashtable[1].key == 1);
+        REQUIRE(hashtable.hashtable[1].psl == 0);
+    }
+
+    SECTION("Domino Swaps from the End"){
+        Robinhood hashtable(32);
+
+        for(int i = 1; i < hashtable.N; ++i){
+            hashtable.insert(i, {static_cast<size_t>(i)});
+        }
+
+        // Everything should have a PSL = 0 
+        for(int i = 1; i < hashtable.N; ++i){
+            REQUIRE(hashtable.hashtable[i].key == i);
+            REQUIRE(hashtable.hashtable[i].psl == 0);
+        }
+
+        // The first position should be empty, we are going to add something soon though
+        REQUIRE(!hashtable.hashtable[0].occupied);
+
+        hashtable.insert(33, {static_cast<size_t>(33)});
+
+        // The "first" one escapes bc they have the same PSL, but the "second" one NO, so it begins!
+        REQUIRE(hashtable.hashtable[1].key == 1);
+        REQUIRE(hashtable.hashtable[1].psl == 0);
+
+        REQUIRE(hashtable.hashtable[2].key == 33);
+        REQUIRE(hashtable.hashtable[2].psl == 1);
+
+        for(int i = 3; i < hashtable.N; ++i){
+            REQUIRE(hashtable.hashtable[i].key == (i-1));
+            REQUIRE(hashtable.hashtable[i].psl == 1);
+        }
+
+        // Now 31 should be at pos[0]
+        REQUIRE(hashtable.hashtable[0].key == 31);
+        REQUIRE(hashtable.hashtable[0].psl == 1);
+    }
+
+    SECTION("Inserting a Poor Key into a Rich Cluster"){
+        Robinhood hashtable(32);
+
+        // Hash = 0
+        for(int i = 0; i < hashtable.N-1; ++i){
+            int32_t key = i * hashtable.N;
+            hashtable.insert(key, {static_cast<size_t>(key)});
+        }
+
+        // Because all of them are hashing to the same bucket, PSL = i
+        for(int i = 0; i < hashtable.N-1; i++){
+            REQUIRE(hashtable.hashtable[i].key == (i * hashtable.N));
+            REQUIRE(hashtable.hashtable[i].psl == i);
+        }
+
+        // This one should be free
+        REQUIRE(!hashtable.hashtable[31].occupied);
+
+        hashtable.insert(1, {static_cast<size_t>(1)});
+
+        for(int i = 0; i < hashtable.N -1; i++){
+            REQUIRE(hashtable.hashtable[i].key == (i * hashtable.N));
+            REQUIRE(hashtable.hashtable[i].psl == i);
+        }
+
+        // The "unlucky" one should be on the very end
+        REQUIRE(hashtable.hashtable[31].key == 1);
+        REQUIRE(hashtable.hashtable[31].psl == (hashtable.N - 2));
+    }
+
+    SECTION("Mix Cluster Competition"){
+        Robinhood hashtable(16);
+
+        // Inserting 3 with different hash (0, 1 and 2 respectively)
+        hashtable.insert(16, {static_cast<size_t>(16)});
+        hashtable.insert(1, {static_cast<size_t>(1)});
+        hashtable.insert(2, {static_cast<size_t>(2)});
+
+        REQUIRE(hashtable.hashtable[0].psl == 0);
+        REQUIRE(hashtable.hashtable[1].psl == 0);
+        REQUIRE(hashtable.hashtable[2].psl == 0);
+        
+        // Hash 0
+        hashtable.insert(32, {static_cast<size_t>(32)});
+
+        /*      key: 16, 32, 1, 2
+                psl:  0,  1, 1, 1       */
+        
+        REQUIRE(hashtable.hashtable[0].key == 16);
+        REQUIRE(hashtable.hashtable[1].key == 32);
+        REQUIRE(hashtable.hashtable[2].key == 1);
+        REQUIRE(hashtable.hashtable[3].key == 2);
+
+        // Hash 1
+        hashtable.insert(17, {static_cast<size_t>(17)});
+
+        /*      key: 16, 32, 1, 17, 2
+                psl:  0,  1, 1,  2, 2   */
+        
+        REQUIRE(hashtable.hashtable[2].key == 1);
+        REQUIRE(hashtable.hashtable[3].key == 17);
+        REQUIRE(hashtable.hashtable[4].key == 2);
+
+        // Hash 2
+        hashtable.insert(18, {static_cast<size_t>(18)});
+
+        /*      key: 16, 32, 1, 17, 2, 18
+                psl:  0,  1, 1,  2, 2,  3       */
+        
+        REQUIRE(hashtable.hashtable[5].key == 18);
+        REQUIRE(hashtable.hashtable[5].psl == 3);
+
+    }
+}
+
